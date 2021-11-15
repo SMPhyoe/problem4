7252a0a5d178a940d37dfa911377a0239f8d3b4f
SMPhyoe <sumyatphyoe.qqsm@gmail.com>
2021-11-15 14:47:43 +0800

    commit

---
+++ bpiconsumer/Models/BPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bpiconsumer.Models
+{
+    public class BPI
+    {
+        public string UpdatedTime { get; set; }
+        public string ChartName { get; set; }
+        public string USD_RATE { get; set; }
+        public string USD_RATE_FLOAT { get; set; }
+    }
+}
